54faa52aac763bf908aff1939769ed1fdd555456
Vadim Yalovenko <yalovek@ya.ru>
2015-12-13 14:06:00 +0300

    Optimization

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-module.exports = {
-    // TODO: в бемхинте нет возможности не указывать технологии
-    forEntityTech: function (tech, techsConfig, entity) {
-        if (tech.content === '') {
-            var error = {
-                msg: 'Empty file',
-                tech: tech.tech
-            };
-
-            entity.addError(error);
-        }
+module.forEntityTech = function (tech, techsConfig, entity) {
+    if (!tech.content) {
+        entity.addError({
+            msg: 'Empty file',
+            tech: tech.tech
+        });
     }
 };
